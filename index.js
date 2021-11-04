8db4950d24f0d235d10f47339b664ffa670d0ae5
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2021-11-04 08:13:06 +0600

    insert add

--- index.js
+++ index.js
@@ -1,60 +1,47 @@
 const express = require('express');
-const { MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// user : myPracticeDB
-// password : ly7Pyn69aCXuIWHy
-
-
-
-
-const uri = "mongodb+srv://myPracticeDB:ly7Pyn69aCXuIWHy@cluster0.zl5hq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zl5hq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db("SexMaster");
-    const usersCollection = database.collection("users");
 
-    //GET API
-    app.get('/users', async(req, res)=>{
-      const cursor = usersCollection.find({});
-      const users = await cursor.toArray();
-      res.send(users);
-    });
-    
-    // Post API
-    app.post('/users', async (req, res)=>{
-      const newUser = req.body;
-      const result = await usersCollection.insertOne(newUser)
-      console.log('hitting the post', req.body);
-      console.log('added user', result);
-      res.json(result);
-    });
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('design_printing');
+        const serviceCollection = database.collection('services');
 
-    // DELETE API
-    app.delete('users/:id', async(req, res)=>{
-      const id = req.params.id;
-      const query = {_id: ObjectId(id)};
-      const result = await usersCollection.deleteOne(query);
-      console.log('deleting user with id', result);
-      res.json(result);
-    });
+        // GET services
+        app.get('/services', async(req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
 
-  } finally {
-    // await client.close();
-  }
+        // POST API
+        app.post('/servicesdata', async(req, res)=>{
+            const servicesdata = req.body;
+            console.log('hit the post api', servicesdata);
+
+            const result = await serviceCollection.insertOne(servicesdata);
+            console.log(result);
+            res.json(result);
+        } )
+    }
+    finally{
+      // await client.close();
+    }
 }
-run().catch(console.dir);
 
+run().catch(console.dir);
 
 app.get('/', (req, res) =>{
     res.send('Hello mongodb, I am success install');
